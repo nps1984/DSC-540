e835c3a44f74f7bcfb454d19ce2ee005c4e17879
Nicholas Salisbury <salisbury.nick@gmail.com>
2020-05-28 21:06:22 -0400

    remove the old week1 from the repo as not needed

--- Week1/DSC540_Week1_Salisbury_line-chart.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-pop_file = 'world-population.xlsm'
-world_pop = pd.read_excel(pop_file)
-
-world_pop.head()
-world_pop.plot(x='Year',y='Population')
-plt.show()
